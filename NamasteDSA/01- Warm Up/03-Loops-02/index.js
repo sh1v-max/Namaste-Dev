6a8b7ec919988500c99a9affa919f2935f871b3d
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-13 19:56:30 +0530

    loops

--- NamasteDSA/01- Warm Up/03-Loops-02/index.js
+++ NamasteDSA/01- Warm Up/03-Loops-02/index.js
@@ -34,7 +34,7 @@ let result2 = countNegativeNumbers(arr2)
 // write a function that returns the largest number in an array
 
 function findLargest(arr) {
-  let largest = 0
+  let largest = -Infinity
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > largest) {
       largest = arr[i]
@@ -45,4 +45,21 @@ function findLargest(arr) {
 
 let arr3 = [2, -6, 4, 8, 1, -9]
 let result3 = findLargest(arr3)
-console.log(result3)  
+// console.log(result3)
+
+
+// write a function that returns the smallest number in an array
+
+function findSmallest(arr) {
+  let smallest = Infinity
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i]
+    }
+  }
+  return smallest
+}
+
+let arr4 = [2, -6, 4, 8, 1, -9]
+let result4 = findSmallest(arr4)
+console.log(result4)
