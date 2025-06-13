323416b84f945cabc87933dadb35553a4b5841da
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-13 21:04:07 +0530

    loops

--- NamasteDSA/01- Warm Up/04-Second-largest/optimized secend largest number.js
+++ NamasteDSA/01- Warm Up/04-Second-largest/optimized secend largest number.js
@@ -7,6 +7,24 @@ function secondLargest(arr) {
     if (arr[i] > first) {
       second = first;
       first = arr[i];
+    } else if (arr[i] > second) {
+      second = arr[i];
+    }
+  }
+  return second === -Infinity ? "No second largest found" : second;
+}
+
+console.log(secondLargest([0, 3, 5, 2, 7, 9])); // 7
+
+
+// what if array consist of 2 same elements
+function secondLargest(arr) {
+  if (arr.length < 2) return "Array should have at least two numbers";
+  let first = -Infinity, second = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > first) {
+      second = first;
+      first = arr[i];
     } else if (arr[i] > second && arr[i] !== first) {
       second = arr[i];
     }
