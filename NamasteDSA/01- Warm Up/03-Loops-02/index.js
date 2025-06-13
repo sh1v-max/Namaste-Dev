c1407bb62b25e95af88708489e716b93d0a39730
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-13 19:28:06 +0530

    loops question

--- NamasteDSA/01- Warm Up/03-Loops-02/index.js
+++ NamasteDSA/01- Warm Up/03-Loops-02/index.js
@@ -1,71 +1,32 @@
-// Loops are used to **run the same block of code repeatedly** with different values.
+// write a function that searches for an element in an array and returns its index
+// if the element is not found, return -1
 
-// **`for` Loop**
-// Used when you know how many times you want to loop.
-for (let i = 0; i < 5; i++) {
-  console.log(i);  // prints 0 to 4
-}
-
-// **`while` Loop**
-// Used when you don’t know how many times you'll loop in advance.
-let j = 0;
-while (j < 5) {
-  console.log(j);
-  j++;
-}
-
-// **`do...while` Loop**
-// Same as `while`, but the code block is executed at least once.
-let i = 0;
-do {
-  console.log(i);
-  i++;
-} while (i < 5);
-
-// **`for...in` Loop**
-// Used to loop over the **keys** of an object.
-const person = { name: "Wazir", age: 23 };
-for (const key in person) {
-  console.log(key);  // prints name and age
-}
-
-// **`for...of` Loop**
-// Used to loop over the **values** of an object or array.
-const numbers = [1, 2, 3];
-for (const number of numbers) {
-  console.log(number);  // prints 1, 2, 3
-}
-
-// =====================================================
-// examples
-// Print numbers from 1 to 10
-for (let i = 1; i <= 10; i++) {
-  console.log(i);
-}
-
-let arr = [10, 5, 7, 0, 3, 8, 80]
-
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
-
-// all even numbers
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] % 2 === 0) {
-    console.log(arr[i]);
+function searchElement(arr, element) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === element) {
+      return i
+    }
   }
+  return -1
 }
 
-// sum
-let sum = 0;
-for (let i = 1; i <= 10; i++) {
-  sum += i;
-}
-console.log(sum);
+let arr = [1, 2, 3, 4, 5]
+let result = searchElement(arr, 5)
+// console.log(result)
 
-// while
-let a = 0
-while (a < 5) {
-  console.log(`hello ${a}`);
-  a++
+// wrote a function that returns the number of negative numbers in an array
+
+function countNegativeNumbers(arr) {
+  let count = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      count++
+    }
+  }
+  // console.log(count)
+  return count
 }
+
+let arr2 = [2, -6, 4, 8, 1, -9]
+let result2 = countNegativeNumbers(arr2)
+console.log(result2)
