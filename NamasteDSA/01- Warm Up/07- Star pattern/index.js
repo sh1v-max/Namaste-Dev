532ec601f90b84de598d8a7e97ccade7c35f0683
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-14 20:10:58 +0530

    pattern 3

--- NamasteDSA/01- Warm Up/07- Star pattern/index.js
+++ NamasteDSA/01- Warm Up/07- Star pattern/index.js
@@ -33,4 +33,23 @@ function pattern2(n) {
   }
 }
 
-pattern2(5)
+// pattern2(5)
+
+// pattern 3
+// 1  
+// 1 2  
+// 1 2 3  
+// 1 2 3 4  
+// 1 2 3 4 5  
+
+function pattern3(n) {
+  for (let i = 0; i < n; i++) {
+    let row = ''
+    for (let j = 1; j <= i; j++) {
+      row += j + " " 
+    }
+    console.log(row)
+  }
+}
+
+pattern3(5)
