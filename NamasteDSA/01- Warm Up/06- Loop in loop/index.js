913733528bf3c0bf48aaa64d675a4805b78b73bc
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-14 18:20:36 +0530

    more practice

--- NamasteDSA/01- Warm Up/06- Loop in loop/index.js
+++ NamasteDSA/01- Warm Up/06- Loop in loop/index.js
@@ -13,7 +13,7 @@ for (let i = 1; i <= 3; i++) {
 
 for (let i = 1; i <= 3; i++) {
   for (let j = 1; j <= i; j++) {
-    console.log(`${i} ${j} `);
+    // console.log(`${i} ${j} `);
   }
 }
 // output
@@ -37,3 +37,36 @@ for (let i = 3; i >= 1; i--) {
 // 2 1
 // 1 1
 
+for(let i = 0; i <3; i++){
+  for (let j = i; j > 0; j--){
+    // console.log(i, j);
+  }
+}
+
+// output
+// 1 1
+// 2 2
+// 2 1
+
+for (let i = 5; i> 0; i--){
+  for (let j = 0; j < i; j++){
+    console.log(i, j);
+  }
+}
+
+// output
+// 5 0
+// 5 1
+// 5 2
+// 5 3
+// 5 4
+// 4 0
+// 4 1
+// 4 2
+// 4 3
+// 3 0
+// 3 1
+// 3 2
+// 2 0
+// 2 1
+// 1 0
