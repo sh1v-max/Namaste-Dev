7c2b06a7929296c00f524060ff94f707c7ffd872
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-02 13:19:25 +0530

    intersection in the linked list

---
+++ NamasteDSA/06-Linked-list-level-0/09-Intersection-of-two-linkedList/index.js
@@ -0,0 +1,48 @@
+// Function to find the intersecting node of two linked lists
+
+// approach one, brute force
+// Loop through each node in headA, and for every node, 
+// loop through headB to check if any node is the same by reference.
+
+function getIntersectionNode(headA, headB) {
+  let currA = headA;
+
+  while (currA) {
+    let currB = headB;
+    while (currB) {
+      if (currA === currB) return currA;
+      currB = currB.next;
+    }
+    currA = currA.next;
+  }
+
+  return null;
+}
+
+// Time: O(n × m)
+// Space: O(1)
+
+// using set
+// Store all nodes from headA in a Set (reference, not value)
+// Then traverse headB and check if any node exists in the set
+
+function getIntersectionNode(headA, headB) {
+  const visited = new Set();
+
+  let currA = headA;
+  while (currA) {
+    visited.add(currA);
+    currA = currA.next;
+  }
+
+  let currB = headB;
+  while (currB) {
+    if (visited.has(currB)) return currB;
+    currB = currB.next;
+  }
+
+  return null;
+}
+
+// Time: O(n + m)
+// Space: O(n) for the Set
