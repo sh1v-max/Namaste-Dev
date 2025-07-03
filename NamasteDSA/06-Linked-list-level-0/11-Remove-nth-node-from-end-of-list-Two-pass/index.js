c99c87669630960ebd8ac595fc7c1052848c4c2f
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-03 12:48:10 +0530

    removing previous node using one pass

--- NamasteDSA/06-Linked-list-level-0/11-Remove-nth-node-from-end-of-list-Two-pass/index.js
+++ NamasteDSA/06-Linked-list-level-0/11-Remove-nth-node-from-end-of-list-Two-pass/index.js
@@ -25,5 +25,57 @@ function removeNthFromEnd (head, n) {
   }
   // removing the element
   prev.next = prev.next.next
-  return sentinel.head
+  return sentinel.next
+}
+
+// approach 2: One-pass algorithm
+// using two pointers
+// use a dummy node before the head to handle edge cases easily
+// move the first pointer n steps ahead
+// move both first and second pointer until first reaches the end
+// now second is just before the node to be deleted
+// use second.next = second.next.next to remove the node
+
+function removeNthFromEnd1 (head, n) {
+  // creating the sentinel node
+  let sentinel = new ListNode()
+  sentinel.next = head
+  let first = sentinel
+
+  // moving first n step ahead
+  for (let i = 0; i < n; i++){
+    first = first.next
+  }
+
+  let second = sentinel
+  // moving both pointers until first reaches the end
+  while (first.next){
+    first = first.next
+    second = second.next
+  }
+  // removing the node
+  second.next = second.next.next
+  return sentinel.next
+}
+
+// more optimized solution
+function removeNthFromEnd2 (head, n) {
+  let sentinel = new ListNode()
+  sentinel.next = head
+  // creating first, second and a count variable
+  let first = sentinel
+  let second = sentinel
+  let count = 0
+  // doing everything in one loop
+  // moving first n step ahead, and only moving second if count is greater than n
+  while (first){
+      first = first.next
+      if (count > n) {
+          second = second.next
+      }
+      count++
+  }
+
+  second.next = second.next.next
+  return sentinel.next
 }
