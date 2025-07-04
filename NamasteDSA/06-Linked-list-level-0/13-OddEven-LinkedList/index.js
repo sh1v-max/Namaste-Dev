0e8bd8792a42cdfd421c4ad72e481462273d43b2
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-04 13:04:07 +0530

    odd even linked list done

--- NamasteDSA/06-Linked-list-level-0/13-OddEven-LinkedList/index.js
+++ NamasteDSA/06-Linked-list-level-0/13-OddEven-LinkedList/index.js
@@ -9,18 +9,24 @@
 // link the last odd node to the head of the even nodes
 
 var oddEvenList = function(head) {
+  // check if the list is empty or have one node
+  // return the head, because no need to do anything
   if (!head || !head.next) return head;
   let odd = head;
   let even  = head.next
+  // maintaining another copy of even, so that we can attach it to the end
   let evenHead = even
   
+  // loop until the end of the list, either the last even node, or next to it is null
   while(even && even.next){
     odd.next = odd.next.next
     even.next = even.next.next
+    // update the pointers
     odd = odd.next
     even = even.next
   }
 
+  // link the last odd node to the head of the even node, evenHead
   odd.next = evenHead
   return head
 };
