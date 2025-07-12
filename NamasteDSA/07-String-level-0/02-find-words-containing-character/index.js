a68aa1b748ff69b1035da12d35b2637bfabf26c9
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-12 12:43:04 +0530

    words containin char

---
+++ NamasteDSA/07-String-level-0/02-find-words-containing-character/index.js
@@ -0,0 +1,20 @@
+// given array and a char
+// return array of indexes of words containing char
+
+// approach:
+// two nested loops, outer for words, and inner for char
+// if word contains char, push index in a new array
+// return array
+
+function findWords(words, char) {
+  let res = []
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words[i].length; j++) {
+      if (words[i][j] === char) {
+        res.push(i)
+        break
+      }
+    }
+  }
+  return res
+}
