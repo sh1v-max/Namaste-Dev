cf372a8782d3e61dcd979db9996db5ef8ced6cae
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-24 14:21:56 +0530

    implement stack using one queues

--- NamasteDSA/08-Stack-and-Queues/01-Implement-stack-using-queues/index.js
+++ NamasteDSA/08-Stack-and-Queues/01-Implement-stack-using-queues/index.js
@@ -1,6 +1,7 @@
-// Implementing a Stack (LIFO) using two Queues (FIFO).
+// Implementing a Stack (LIFO) using Queues (FIFO).
+
+// Approach 1: Using Two Queues
 // We maintain two queues q1 (main) and q2 (helper).
-// Approach:
 //   - push(x): Simply push into q1 (O(1)).
 //   - pop(): Move all elements except the last one from q1 to q2. The last element in q1 is the stack's "top" which we pop and return. Then swap q1 and q2.
 //   - top(): Similar to pop(), but instead of removing the last element, we peek at it, push it to q2, and swap q1 and q2.
@@ -12,76 +13,104 @@ var MyStack = function () {
   this.q2 = []
 }
 
-/**
- * @param {number} x
- * @return {void}
- */
 MyStack.prototype.push = function (x) {
   // push the element into the first queue
   this.q1.push(x)
 }
 
-/**
- * @return {number}
- */
 MyStack.prototype.pop = function () {
-  // Step 1: Move all elements except the last one from q1 to q2
+  // Move all elements except the last one from q1 to q2
   // The last element in q1 is the "top" of our stack
   let n = this.q1.length
   // Dequeue from q1 and enqueue into q2
   for (let i = 0; i < n - 1; i++) {
+    // let frontEle = this.q1.shift()
+    // this. q2.push(frontEle)
+    // can also write as
     this.q2.push(this.q1.shift())
   }
-  
-  // Step 2: The last remaining element in q1 is the stack top
+
+  // The last remaining element in q1 is the stack top
   let res = this.q1.shift()
 
   // swap the queues
-  let temp = q1
-  q1 = q2
-  q2 = temp
+  let temp = this.q1
+  this.q1 = this.q2
+  this.q2 = temp
 
   return res
 }
 
-/**
- * @return {number}
- */
-MyStack.prototype.top = function() {
-  // Step 1: Move all elements except the last one from q1 to q2
-  let n = this.q1.length;
+MyStack.prototype.top = function () {
+  // Move all elements except the last one from q1 to q2
+  let n = this.q1.length
   for (let i = 0; i < n - 1; i++) {
-    this.q2.push(this.q1.shift());
+    this.q2.push(this.q1.shift())
   }
 
-  // Step 2: The last element in q1 is the "top"
-  let front = this.q1[0]; // Peek at it
+  // The last element in q1 is the "top"
+  // this is valid, because we've moved all elements from q1 to q2
+  // and accessing the peek
+  let front = this.q1[0]
 
-  // Step 3: Move the last element into q2 (to keep the order intact)
-  this.q2.push(this.q1.shift());
+  // Move the last element into q2
+  // because that's what top says
+  this.q2.push(this.q1.shift())
 
-  // Step 4: Swap q1 and q2
-  let temp = this.q1;
-  this.q1 = this.q2;
-  this.q2 = temp;
+  // Swap q1 and q2
+  let temp = this.q1
+  this.q1 = this.q2
+  this.q2 = temp
 
-  // Step 5: Return the top element
-  return front;
-};
-
-/**
- * @return {boolean}
- */
-MyStack.prototype.empty = function () {
-  // If q1 is empty, the stack is empty
-  return this.q1.length === 0;
+  // Return the top element
+  return front
 }
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * var obj = new MyStack()
- * obj.push(x)
- * var param_2 = obj.pop()
- * var param_3 = obj.top()
- * var param_4 = obj.empty()
- */
+MyStack.prototype.empty = function () {
+  // If q1 is empty, the stack is empty
+  return this.q1.length === 0
+}
+
+// approach 2: using one queue
+// maintain a single queue (q).
+// push(x): Add (enqueue) element to the back of the queue.
+// pop(): Rotate the queue to bring the last pushed element to the front by
+// dequeuing and enqueuing (shift + push) all previous elements.
+// Then dequeue the front (which is the stack top).
+// top(): Same as pop(), but after retrieving the front element,
+// we push it back to keep the queue intact.
+// empty(): Check if queue is empty.
+
+var MyStack = function () {
+  this.q = []
+}
+
+MyStack.prototype.push = function (x) {
+  this.q.push(x)
+}
+
+MyStack.prototype.pop = function () {
+  let n = this.q.length
+  // rotate the queue by removing all elements except the last one
+  // the last pushed element will become the "front" of the queue
+  for (let i = 0; i < n - 1; i++) {
+    this.q.push(this.q.shift())
+  }
+
+  return this.q.shift()
+}
+
+MyStack.prototype.top = function () {
+  let n = this.q.length
+  for (let i = 0; i < n - 1; i++) {
+    this.q.push(this.q.shift())
+  }
+
+  let front = this.q.shift()
+  this.q.push(front)
+  return front
+}
+
+MyStack.prototype.empty = function () {
+  return this.q.length === 0
+}
