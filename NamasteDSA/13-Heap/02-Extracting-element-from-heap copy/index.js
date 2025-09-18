1d88d1c3364f86a4725427ef4a56ae6ee7bded21
sh1v-max <singhshiv0427@gmail.com>
2025-09-18 15:21:47 +0530

    extracting element from heap

--- NamasteDSA/13-Heap/02-Extracting-element-from-heap copy/index.js
+++ NamasteDSA/13-Heap/02-Extracting-element-from-heap copy/index.js
@@ -3,87 +3,87 @@
 
 // creating a heap and helper functions
 class MinHeap {
-    constructor() {
-        // this.heap = [5, 10, 20, 30]
-        this.heap = []
+  constructor() {
+    // this.heap = [5, 10, 20, 30]
+    this.heap = []
+  }
+
+  getLeftChildIndex(i) {
+    return 2 * i + 1
+  }
+
+  getRightChildIndex(i) {
+    return 2 * i + 2
+  }
+
+  getParentIndex(i) {
+    return Math.floor((i - 1) / 2)
+  }
+
+  // ========================================================
+  // for insertion (heapify up)
+  insert(val) {
+    this.heap.push(val)
+    let lastIndex = this.heap.length - 1
+    this.heapifyUp(lastIndex)
+  }
+
+  heapifyUp(i) {
+    while (i > 0) {
+      let parentIndex = this.getParentIndex(i)
+      if (this.heap[i] < this.heap[parentIndex]) {
+        ;[this.heap[i], this.heap[parentIndex]] = [
+          this.heap[parentIndex],
+          this.heap[i],
+        ]
+        i = parentIndex
+      } else {
+        break
+      }
     }
+  }
 
-    getLeftChildIndex(i) {
-        return 2 * i + 1
+  // ========================================================
+  // for extraction (heapify down)
+  extract() {
+    if (this.heap.length < 1) return null
+
+    let min = this.heap[0]
+    let lastIndex = this.heap.length - 1
+
+    ;[this.heap[0], this.heap[lastIndex]] = [this.heap[lastIndex], this.heap[0]]
+
+    this.heap.pop()
+    this.heapifyDown(0)
+    return min
+  }
+
+  heapifyDown(i) {
+    let left = this.getLeftChildIndex(i)
+    let right = this.getRightChildIndex(i)
+    let n = this.heap.length
+
+    // checking for the smallest value among left and right
+    let smallest = i
+    if (right < n && this.heap[right] < this.heap[smallest]) {
+      smallest = right
     }
-
-    getRightChildIndex(i) {
-        return 2 * i + 2
+    if (left < n && this.heap[left] < this.heap[smallest]) {
+      smallest = left
     }
-
-    getParentIndex(i) {
-        return Math.floor((i - 1) / 2)
+    // swapping and then calling heapify down recursively
+    if (smallest != i) {
+      ;[this.heap[smallest], this.heap[i]] = [this.heap[i], this.heap[smallest]]
+      this.heapifyDown(smallest)
     }
+  }
 
-    // ========================================================
-    // for insertion (heapify up)
-    insert(val) {
-        this.heap.push(val)
-        let lastIndex = this.heap.length - 1
-        this.heapifyUp(lastIndex)
-    }
-
-    heapifyUp(i) {
-        while (i > 0) {
-            let parentIndex = this.getParentIndex(i)
-            if (this.heap[i] < this.heap[parentIndex]) {
-                ;[this.heap[i], this.heap[parentIndex]] = [
-                    this.heap[parentIndex],
-                    this.heap[i],
-                ]
-                i = parentIndex
-            } else {
-                break
-            }
-        }
-    }
-
-    // ========================================================
-    // for extraction (heapify down)
-    extract() {
-        if (this.heap.length < 1) return null
-
-        let min = this.heap[0]
-        let lastIndex = this.heap.length - 1
-
-            ;[this.heap[0], this.heap[lastIndex]] = [this.heap[lastIndex], this.heap[0]]
-
-        this.heap.pop()
-        this.heapifyDown(0)
-        return min
-    }
-
-    heapifyDown(i) {
-        let left = this.getLeftChildIndex(i)
-        let right = this.getRightChildIndex(i)
-        let n = this.heap.length
-
-        // checking for the smallest value among left and right
-        let smallest = i
-        if (right < n && this.heap[right] < this.heap[smallest]) {
-            smallest = right
-        }
-        if (left < n && this.heap[left] < this.heap[smallest]) {
-            smallest = left
-        }
-        // swapping and then calling heapify down recursively
-        if (smallest != i) {
-            ;[this.heap[smallest], this.heap[i]] = [this.heap[i], this.heap[smallest]]
-            this.heapifyDown(smallest)
-        }
-    }
-
-    // ========================================================
-    // peak function
-    peak() {
-        if (this.heap.length < 1) return null
-        return this.heap[0]
-    }
+  // ========================================================
+  // peak function
+  peak() {
+    if (this.heap.length < 1) return null
+    return this.heap[0]
+  }
 }
 
 let newHeap = new MinHeap()
@@ -102,7 +102,6 @@ heap2.insert(30)
 heap2.insert(1)
 heap2.insert(0)
 console.log(heap2.heap)
-console.log("extracted val1: " + heap2.extract())
-console.log("extracted val2: " + heap2.extract())
+console.log('extracted val1: ' + heap2.extract())
+console.log('extracted val2: ' + heap2.extract())
 console.log(heap2.heap)
-
