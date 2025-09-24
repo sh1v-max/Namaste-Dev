9884cf27804955abc6df50740d5b23a1065a7285
sh1v-max <singhshiv0427@gmail.com>
2025-09-24 15:11:00 +0530

    insertion sort

--- NamasteDSA/05-Searching-and-sorting-level-0/05-Insertion-sort/index.js
+++ NamasteDSA/05-Searching-and-sorting-level-0/05-Insertion-sort/index.js
@@ -8,26 +8,26 @@
 // Repeat until the whole array is sorted.
 
 function insertionSort(arr) {
-  let n = arr.length;
+  let n = arr.length
 
   for (let i = 1; i < n; i++) {
-    let curr = arr[i];
-    let pre = i - 1;
+    let curr = arr[i]
+    let pre = i - 1
 
     // Move elements of arr[0..i-1], that are
     // greater than curr, to one position ahead
     // of their current position
     while (pre >= 0 && arr[pre] > curr) {
-      arr[pre + 1] = arr[pre];
-      pre = pre - 1;
+      arr[pre + 1] = arr[pre]
+      pre = pre - 1
     }
-    arr[pre + 1] = curr;
+    arr[pre + 1] = curr
   }
-  return arr;
+  return arr
 }
 
 let arr = [5, 4, 3, 2, 1]
-console.log(insertionSort(arr));
+console.log(insertionSort(arr))
 
 // Time Complexity: O(n^2)
 // Space Complexity: O(1)
