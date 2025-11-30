db5980dde8cc524e30bc56fbe63151e3c7b599ed
sh1v-max <singhshiv0427@gmail.com>
2025-11-30 15:48:40 +0530

    priority queue, last stone weight

--- NamasteDSA/14-Priority-queue/04-Last-stone-weight/index.js
+++ NamasteDSA/14-Priority-queue/04-Last-stone-weight/index.js
@@ -18,19 +18,7 @@
 // we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
 // we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
 
-// example 2:
-// Input: stones = [1]
-// Output: 1
-
-// example 3:
-// Input: stones = []
-// Output: 0
-
-// intuition:
-// at each turn, we want the two heaviest stones
-// smashing two largest stones reduces the problem size by 1 or 2
-// max heap allows us to always get the largest two stones efficiently
-
+// with heap:
 // example: stones = [2,7,4,1,8,1]
 // heap = [8,7,4,2,1,1]
 // - take 8 and 7 → 1 left → heap = [4,2,1,1,1]
@@ -39,14 +27,23 @@
 // - take 1 and 1 → 0 left → heap = [1]
 // return 1
 
+// example 2:
+// Input: stones = [1]
+// Output: 1
+
+// example 3:
+// Input: stones = []
+// Output: 0
+
 // heap / priority queue — greedy
 // intuition:
 // the problem is about repeatedly taking the two heaviest stones and smashing them
 // - if they’re equal, both are destroyed
 // - if not, the smaller is destroyed, and the larger is reduced by the smaller's weight
 // at each step, you always want to pick the heaviest stones to reduce the array optimally
-// this is a classic **max-heap (priority queue) scenario**, where we always extract the largest elements first
+// this is a classic max-heap (priority queue) scenario, where we always extract the largest elements first
 // continuing this process until <= 1 stone remains guarantees correct final weight
+// max heap allows us to always get the largest two stones efficiently
 
 // approach:
 // - create a max-heap (priority queue) of stones
@@ -55,7 +52,6 @@
 //       - if x != y, insert (y - x) back into heap
 //       - if x == y, do nothing (both destroyed)
 // - return the last remaining stone (heap top) or 0 if none remain
-
 
 var lastStoneWeight = function (stones) {
   let pq = new MaxPriorityQueue()
