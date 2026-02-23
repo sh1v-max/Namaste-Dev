a73585ea2c68f3ce6a9debc80b8374b06b8208db
sh1v-max <singhshiv0427@gmail.com>
2026-02-23 21:55:59 +0530

    solved: prefix sum, find the pivot integer

--- NamasteDSA/00-Practice-Problems/04-Prefix-sum/D7-2485-Find-the-pivot-integer/index.js
+++ NamasteDSA/00-Practice-Problems/04-Prefix-sum/D7-2485-Find-the-pivot-integer/index.js
@@ -61,15 +61,15 @@ var pivotInteger = function (n) {
 //       return x
 // return -1
 
-var pivotInteger = function(n) {
+var pivotInteger = function (n) {
   let totalSum = (n * (n + 1)) / 2
   let leftSum = 0
 
   for (let x = 1; x <= n; x++) {
     leftSum += x
-    
+
     let rightSum = totalSum - (leftSum - x)
-    
+
     if (leftSum === rightSum) {
       return x
     }
